d39bb14d849ac68238b466144ea4e71d12464f09
cmcgrud1 <cmcgrud1@vols.utk.edu>
2019-08-17 20:13:27 -0400

    edits to add transition temps between molecules

--- plot_access_targets.py
+++ plot_access_targets.py
@@ -9,7 +9,6 @@ def calc_T_eq(T_star, R_star, distance, albedo):
     """
     Calculate the equilibrium temperature of a planet
     with a given albedo at a given distance from a star
-
     Inputs:
         T_star   : effective temperature of star in K
         R_star   : radius of star in solar units
@@ -41,10 +40,27 @@ df2 = pd.read_csv('targets.csv')
 
 ################################################################################
 # Make figure
-fig, ax = plt.subplots(figsize=(6.5, 3))
+fig, ax = plt.subplots(figsize=(25, 15))
 ax.set_title('ACCESS Targets')
 ax.set_xlabel('Equlibrium Temperature (K)')
 ax.set_ylabel('Planetary Radius ($R_{Jup}$)')
+
+# transition temperatures between important molecules in exoplanet atmospheres. Gotten from Ben's 2016 ACCESS poster
+H2O = 300
+NH3_N2 = 600
+CH4_CO = 1000
+MnS = 1300
+Silicates_MetalOxides = [1600,1900]
+ax.axvline(x = H2O, ls = '--', color ='k', alpha=0.5)
+ax.text(H2O-35, 1.3, '$H_2O$')
+ax.axvline(x = NH3_N2, ls = '--', color ='k', alpha=0.5)
+ax.text(NH3_N2-70, 1.5, '$NH_3 -> N_2$')
+ax.axvline(x = CH4_CO, ls = '--', color ='k', alpha=0.5)
+ax.text(CH4_CO-70, 1.6, '$CH_4 -> CO$')
+ax.axvline(x = MnS, ls = '--', color ='k', alpha=0.5)
+ax.text(MnS-30, 1.75, 'MnS')
+ax.axvspan(Silicates_MetalOxides[0], Silicates_MetalOxides[1], alpha=0.25, color='k')
+ax.text(np.mean(Silicates_MetalOxides)-150, .6, 'Silicates/Metal-oxides')
 
 # Plot all exoplanets
 ax.plot(df.eq_temp, df.radius,
@@ -73,6 +89,7 @@ ax.plot([], [], ls='', marker='o', mec='k', mfc='C2', ms=10, label='Published')
 ax.plot([], [], ls='', marker='o', mec='k', mfc='C0', ms=10, label='Paper in Prep.')
 ax.plot([], [], ls='', marker='o', mec='k', mfc='C1', ms=10, label='Analysis Underway')
 ax.plot([], [], ls='', marker='o', mec='k', mfc='gray', ms=10, label='Collecting Data')
+ax.plot([], [], ls='--', color='k', ms=4, label='Temp. transition of molecules', alpha=0.5)
 ax.legend(loc='upper left')
 
 plt.savefig('access_targets.pdf', bbox_inches='tight')
